b65cbb0652b69e607a6e34b24a57f1c44f67eeec
etomzie <evantw0703@gmail.com>
2026-05-06 00:16:05 +0800

    sdsd

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-#!/usr/local/bin/python3.4
 import pygame, sys
 from pygame.locals import *
 
-
-
-
-
-
-tileSize = 32
+tileSIZE = 32
 
 grid = [
-
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 0 
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 2, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 2, 2, 2, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 2, 2, 2, 0, 0, 0],
-    [0, 0, 0, 1,-1, 1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 2, 2, 2, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 1,-1, 1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 2, 2, 2, 0, 0, 0], # 7
+    [0, 0, 0, 1, 1, 1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 2, 2, 2, 0, 0, 0], # 8
     [0, 0, 0, 1, 1, 1, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,94 +21,135 @@ grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+spawn_point = (4 * tileSIZE, 7 * tileSIZE)
+direction = [(1, 0), (-1, 0)]
+
 tileROW = len(grid) 
 tileCOL = len(grid[0]) 
 
 print(tileROW)
 print(tileCOL)
 
-
-
-
 class Player():
     def __init__(self):
         self.x = -1
         self.y = -1
-        self.speed = 0.2
+        self.speed = 170
         
         self.size = 0.8
-        
     
-    def update(self):
+    def check_TL(self, x, y):
+        ts = tileSIZE
+        return grid[int(y / ts)][int(x / ts)] == 0     
+        
+    def check_TR(self, x, y):
+        ts = tileSIZE
+        x = x + ts * self.size
+        return grid[int(y / ts)][int(x / ts)] == 0     
+    
+    def check_BL(self, x, y):
+        ts = tileSIZE
+        y = y + ts * self.size
+        return grid[int(y / ts)][int(x / ts)] == 0        
+    
+    def check_BR(self, x, y):
+        ts = tileSIZE
+        x = x + ts * self.size
+        y = y + ts * self.size
+        return grid[int(y / ts)][int(x / ts)] == 0        
+    
+    def update(self, dt):
 
         
         keys=pygame.key.get_pressed()
+        ts = tileSIZE
+        
+        offset = 0.01
+        
         if keys[pygame.K_a]:
-            self.x -= self.speed
-            rx -= self.speed
-            lx -= self.speed
-            
-        #if grid[int(self.y // tileSize)][int((self.x) // tileSize)] == 0:
-        #    self.x = int((self.x + tileSize * self.size) // tileSize) * tileSize + tileSize * self.size - tileSize * self.size
-                
-                
+            self.x -= self.speed * dt
+            if self.check_TL(self.x + offset, self.y + offset) or self.check_BL(self.x + offset, self.y - offset):
+                self.x = round(self.x / ts) * ts
+
         if keys[pygame.K_d]:
-            self.x += self.speed
-            rx += self.speed
-            lx += self.speed
-            
-        #    if grid[int(self.y // tileSize)][int((self.x + tileSize * self.size) // tileSize)] == 0:
-        #        self.x = int((self.x + tileSize * self.size) // tileSize) * tileSize + tileSize * (1 - self.size) / 2 - tileSize * self.size
-
+            self.x += self.speed * dt
+            if self.check_TR(self.x - offset, self.y + offset) or self.check_BR(self.x - offset, self.y - offset):
+                self.x = round((self.x + ts * self.size) / ts) * ts - ts * self.size
+    
         if keys[pygame.K_w]:
-            self.y -= self.speed
-     
-        #    if grid[int(ty / tileSize)][int(lx / tileSize)] == 0 or grid[int(ty / tileSize)][int(rx / tileSize)] == 0:
-        #        self.y = round(ty / tileSize) * tileSize
-            """
-            if grid[int(ty // tileSize)][int(lx // tileSize)] == 0 or \
-                grid[int(ty // tileSize)][int(rx // tileSize)] == 0:
-                snap_y = round(ty / tileSize) * tileSize
-                if grid[round(ty / tileSize)][round(self.x / tileSize)] != 0:
-                    self.y = snap_y
-            """
+            self.y -= self.speed * dt
+            if self.check_TL(self.x + offset, self.y + offset) or self.check_TR(self.x - offset, self.y + offset):
+                self.y = round(self.y / ts) * ts
+
         if keys[pygame.K_s]:
-            self.y += self.speed
-
-        #    if grid[int(by // tileSize)][int(lx // tileSize)] == 0 or \
-        #        grid[int(by // tileSize)][int(rx // tileSize)] == 0:
-        #        snap_y = int(by // tileSize) * tileSize + tileSize * (1 - self.size) / 2 - tileSize * self.size
-        #        if grid[int(snap_y // tileSize)][int(self.x // tileSize)] != 0:
-        #            self.y = snap_y
-        
-        lx = self.x
-        rx = self.x + tileSize * self.size
-        ty = self.y
-        by = self.y + tileSize * self.size
-        
-        if grid[int(by / tileSize)][int(lx / tileSize)] == 0:
+            self.y += self.speed * dt
+            if self.check_BL(self.x + offset, self.y + offset) or self.check_BR(self.x - offset, self.y - offset):
+                self.y = round((self.y + ts * self.size) / ts) * ts - ts * self.size
             
-        
 
-        
+
+ 
         
         
     def draw(self, screen):
-        cntr_x = self.x + (tileSize * self.size / 2)
-        cntr_y = self.y + (tileSize * self.size / 2)
+        """
+        cntr_x = self.x + (tileSIZE * self.size / 2)
+        cntr_y = self.y + (tileSIZE * self.size / 2)
+        """
+        pygame.draw.rect(screen, "#1e1e1e", (self.x, self.y
+                                             , tileSIZE * self.size, tileSIZE * self.size))
+        pygame.draw.rect(screen, "#ff0000", (self.x + 5, self.y + 5
+                                             , tileSIZE * self.size - 10, tileSIZE * self.size - 10))        
+            
+class Enemy():
+    def __init__(self,
+                posA, posB,
+                direc_idx):
+        self.posA = posA
+        self.posB = posB
         
-        pygame.draw.rect(screen, "#1e1e1e", (cntr_x - (tileSize * self.size / 2), cntr_y - (tileSize * self.size / 2)
-                                             , tileSize * self.size, tileSize * self.size))
-        pygame.draw.rect(screen, "#ff0000", (cntr_x - (tileSize * self.size / 2) + 5, cntr_y - (tileSize * self.size / 2) + 5
-                                             , tileSize * self.size - 10, tileSize * self.size - 10))
+        self.x = posA[0]
+        self.y = posA[1]
+         
+        self.direc_idx = direc_idx
+        
+        self.speed = 300
+        self.size = 0.3
         
         
 
+    def update(self, dt):
+        self.x += direction[self.direc_idx][0] * self.speed * dt
+        self.y += direction[self.direc_idx][1] * self.speed * dt
+        
+        if grid[int(self.y / tileSIZE)][int(self.x / tileSIZE)] == 0 or grid[int(self.y / tileSIZE)][int(self.x / tileSIZE) + 1] == 0:
+            self.direc_idx = (self.direc_idx + 1) % len(direction)
+            self.x += direction[self.direc_idx][0] * self.speed * dt
+
+    def draw(self, screen):
+        cntr_x = self.x + tileSIZE / 2
+        cntr_y = self.y + tileSIZE / 2
+        
+        pygame.draw.circle(screen, "#000000", (cntr_x, cntr_y), tileSIZE * self.size)
+        pygame.draw.circle(screen, "#0000ff", (cntr_x, cntr_y), tileSIZE * self.size - 5)
+        
+def intersects(rect, r, center):
+    circle_distance_x = abs(center[0]-rect.centerx)
+    circle_distance_y = abs(center[1]-rect.centery)
+    if circle_distance_x > rect.w/2.0+r or circle_distance_y > rect.h/2.0+r:
+        return False
+    if circle_distance_x <= rect.w/2.0 or circle_distance_y <= rect.h/2.0:
+        return True
+    corner_x = circle_distance_x-rect.w/2.0
+    corner_y = circle_distance_y-rect.h/2.0
+    corner_distance_sq = corner_x**2.0 +corner_y**2.0
+    return corner_distance_sq <= r**2.0  
 
 
 
-windowHeight = tileSize * tileROW
-windowWidth = tileSize * tileCOL
+
+windowHeight = tileSIZE * tileROW
+windowWidth = tileSIZE * tileCOL
 
 pygame.init()
 screen = pygame.display.set_mode((windowWidth, windowHeight))
@@ -124,8 +159,19 @@ pygame.display.set_caption('World Hardest Game')
 
 
 player = Player()
+enemies = [Enemy((16 * tileSIZE, 8 * tileSIZE), (7 * tileSIZE, 8 * tileSIZE), 1),
+           Enemy((7 * tileSIZE, 7 * tileSIZE), (16 * tileSIZE, 7 * tileSIZE), 1),
+           Enemy((16 * tileSIZE, 6 * tileSIZE), (7 * tileSIZE, 6 * tileSIZE), 1),
+           Enemy((7 * tileSIZE, 5 * tileSIZE), (16 * tileSIZE, 5 * tileSIZE), 1),
+           ]
+
+clock = pygame.time.Clock()
+
+
 
 while 1:
+    dt = clock.tick(60) / 1000.0 
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -136,32 +182,42 @@ while 1:
     for i in range(tileROW):
         for j in range(tileCOL):
             if grid[i][j] == -1 and (player.y < 0 or player.x < 0):
-                player.y = i * tileSize
-                player.x = j * tileSize
+                player.y = i * tileSIZE
+                player.x = j * tileSIZE
             
             
             if grid[i][j] in {1, 2, -1}:
-                pygame.draw.rect(screen, "#b5feb4", (tileSize * j, tileSize * i, tileSize, tileSize))
+                pygame.draw.rect(screen, "#b5feb4", (tileSIZE * j, tileSIZE * i, tileSIZE, tileSIZE))
             elif grid[i][j] == 3:
                 if (i + j) % 2 == 0:
-                    pygame.draw.rect(screen, "#e6e6ff", (tileSize * j, tileSize * i, tileSize, tileSize))
+                    pygame.draw.rect(screen, "#e6e6ff", (tileSIZE * j, tileSIZE * i, tileSIZE, tileSIZE))
                 else:
-                    pygame.draw.rect(screen, "#f7f7ff", (tileSize * j, tileSize * i, tileSize, tileSize))
+                    pygame.draw.rect(screen, "#f7f7ff", (tileSIZE * j, tileSIZE * i, tileSIZE, tileSIZE))
                     
             
             if grid[i][j] != 0:
                 if i == 0 or grid[i - 1][j] == 0:
-                    pygame.draw.line(screen, "#1e1e1e", (tileSize * j, tileSize * i), (tileSize * (j + 1), tileSize * i), 4)
+                    pygame.draw.line(screen, "#1e1e1e", (tileSIZE * j, tileSIZE * i), (tileSIZE * (j + 1), tileSIZE * i), 4)
                 if i == tileROW - 1 or grid[i + 1][j] == 0:
-                    pygame.draw.line(screen, "#1e1e1e", (tileSize * j, tileSize * (i + 1)), (tileSize * (j + 1), tileSize * (i + 1)), 4)
+                    pygame.draw.line(screen, "#1e1e1e", (tileSIZE * j, tileSIZE * (i + 1)), (tileSIZE * (j + 1), tileSIZE * (i + 1)), 4)
                     
                 if j == 0 or grid[i][j - 1] == 0:
-                    pygame.draw.line(screen, "#1e1e1e", (tileSize * j, tileSize * i), (tileSize * (j), tileSize * (i + 1)), 4)
+                    pygame.draw.line(screen, "#1e1e1e", (tileSIZE * j, tileSIZE * i), (tileSIZE * (j), tileSIZE * (i + 1)), 4)
                 if i == tileCOL - 1 or grid[i][j + 1] == 0:
-                    pygame.draw.line(screen, "#1e1e1e", (tileSize * (j + 1), tileSize * i), (tileSize * (j + 1), tileSize * (i + 1)), 4)
+                    pygame.draw.line(screen, "#1e1e1e", (tileSIZE * (j + 1), tileSIZE * i), (tileSIZE * (j + 1), tileSIZE * (i + 1)), 4)
     
-    player.update()
+    player.update(dt)
     player.draw(screen)
+    
+    for now in enemies:
+        now.update(dt)
+        now.draw(screen)
+
+        if intersects(pygame.Rect(player.x, player.y, tileSIZE * player.size, tileSIZE * player.size), tileSIZE * now.size, (now.x + tileSIZE / 2, now.y + tileSIZE / 2)):
+            player.x = spawn_point[0]
+            player.y = spawn_point[1]
+    
+        
                 
     
     pygame.display.update()
